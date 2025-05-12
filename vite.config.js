23098c54124a8a1b245f286a4f558eea81669ced
ZTZFish <1963109104@qq.com>
2025-05-12 11:09:44 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+//引入腾讯组件库
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { TDesignResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(),
+  // 引入腾讯组件库
+  AutoImport({
+    resolvers: [TDesignResolver({
+      library: 'mobile-vue'
+    })],
+  }),
+  Components({
+    resolvers: [TDesignResolver({
+      library: 'mobile-vue'
+    })],
+  }),
+  ],
+  //
+})
